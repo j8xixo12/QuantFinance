14a05ad0080ac9c3f11124c13b13bd8bed2893e1
j8xixo12 <as2266317@gmail.com>
2020-03-25 23:30:49 +0800

    Add Perepetual Option

--- include/Option.hpp
+++ include/Option.hpp
@@ -1,19 +1,41 @@
 #ifndef Option_hpp_
 #define Option_hpp_
+#include <cmath>
 
-class Option
-{
-public:
-    explicit Option(const double r, const double sig, const double K,
-                    const double T_)
-                    : r_(r), sig_(sig), K_(K), T_(T_) {};
+class Option {
+	public:
+		Option() : r_(0.0), sig_(0.0), K_(0.0), T_(0.0), S_(0.0), b_(0.0) {};
+		explicit Option(const double r, const double sig, const double K,
+						const double T_)
+						: r_(r), sig_(sig), K_(K), T_(T_) {};
 
-	double r_;		// Interest rate
-	double sig_;		// Volatility
-	double K_;		// Strike price
-	double T_;		// Expiry date
-	// double U_;		// Current underlying price (e.g. stock, forward)
-	// double b_;		// Cost of carry
+		double r_;		// Interest rate
+		double sig_;		// Volatility
+		double K_;		// Strike price
+		double T_;		// Expiry date
+		double S_;		// Current underlying price (e.g. stock, forward)
+		double b_;		// Cost of carry
+		double q_;
 };
 
+class PerpetualOption : public Option {
+	public:
+		double PutPrice() {
+			double sig2 = sig_ * sig_;
+			double fac = b_ / sig2 - 0.5; 
+			fac *= fac;
+			double y1 = 0.5 - b_ / sig2 + std::sqrt(fac + 2.0*r_ / sig2);
+			double fac2 = ((y1 - 1.0)*S_) / (y1 * K_);
+			return K_ * std::pow(fac2, y1) / (y1 - 1.0);
+		}
+
+		double CallPrice() {
+			double sig2 = sig_ * sig_;
+			double fac = b_ / sig2 - 0.5; 
+			fac *= fac;
+			double y2 = 0.5 - b_ / sig2 - std::sqrt(fac + 2.0*r_ / sig2);
+			double fac2 = ((y2 - 1.0) * S_) / (y2 * K_);
+			return K_ * std::pow(fac2, y2) / (1.0 - y2);
+		}
+};
 #endif // Option_hpp_
