07a08615eccbcf422d0cabee031b90fc943144e9
j8xixo12 <as2266317@gmail.com>
2020-03-28 17:22:04 +0800

    Add CubicSpline example

--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,60 @@
-#include <vector>
+#include "CubicSpline.hpp"
+#include "utility.hpp"
+#include <random>
 #include <iostream>
-#include <cmath>
-#include <chrono>
-#include "LUTridiagonalSolver.hpp"
-#include "DoubleSweep.hpp"
 
-double InitialCondition(double x) {
-    if (x >= 0.0 && x <= 0.5) {
-        return 2.0 * x; 
-    }
-    return 2.0 * (1.0 - x); 
+
+double Pdf(double x) { // Probability function for Gauss fn.
+    double A = 1.0 / sqrt(2.0 * 3.14159265358979323846);
+    return A * exp(-x*x*0.5); 
 }
-template<typename T>
-using Vector = std::vector<T>;
+double dPdfdx(double x) { // 1st derivative of probability function for Gauss fn.
+    return -x * Pdf(x); 
+}
+double d2Pdfdx2(double x) { // 2nd derivative of probability function for Gauss fn.
+    return -Pdf(x) + x*x*Pdf(x); 
+}
+double Cdf(double x) { // The approximation to the cumulative normal distribution
+// C++11
+    return 0.5* (1.0 + std::erf(x / std::sqrt(2.0))); 
+}
 
-int main(int argc, char *argv[]) {
-    // BTCS scheme for the heat equation
-    long J = 20;
-    long N = 100;
-    long choice = 1;
-    double theta = (choice == 2) ? 0.5 : 1.0;
-    double T = 1.0;
+int main(int argc, char* argv[]) {
+    // Infinite interval, truncated to [a,b]
+    double a = -6.0;
+    double b = 6.0;
+    std::size_t n = 200000;
+    // double h = (b - a) / static_cast<double>(n);
+    std::vector<double> xarr = CreateMesh(n, a, b);
 
-    double A = 0.0; // LHS
-    double B = 1.0; // RHS
-    double h = (B - A) / static_cast<double>(J); 
-    double k = T / static_cast<double>(N);
-    // Boundary conditions 
-    double lambda = k / (h * h);
+    auto fun = Pdf;
+    auto yarr = CreateDiscreteFunction(xarr, fun);
+    // Generate a random number in [a,b] 
+    std::default_random_engine eng; 
+    std::random_device rd; 
+    eng.seed(rd());
+    std::uniform_real_distribution<double> dist(a, b);
+    double xvar = dist(eng);
 
-    // Constructors with size, start index and value (Diagonals of matrix) 
-    // J intervals, thus J-1 UNKNOWN internal points
-    // Dirichlet boundary conditions
-    Vector<double> a(J-1,-lambda*theta);
-    Vector<double> b(J-1,(1.0 + 2.0*lambda*theta));
-    Vector<double> c(J-1,-lambda*theta);
-    Vector<double> r(J-1, 0); // Right-hand side NOT CONSTANT ANYMORE
-                                // Boundary conditions into consideration
-    // Create mesh in space
-    Vector<double> xarr(J + 1); 
-    xarr[0] = A;
-    for (std::size_t j = 1; j < xarr.size(); ++j) {
-        xarr[j] = xarr[j - 1] + h; 
+    CubicSplineInterpolator csi(xarr, yarr, SecondDeriv);
+    try {
+        double result = csi.Solve(xvar);
+        std::cout << "Interpolated value at " << xvar << " " << std::setprecision(16) << result << ", is " << fun (xvar) << std::endl;
+        std::cout << "Integral: " << csi.Integral() << std::endl; 
+    } catch (std::exception& e) { // Catch not in range values
+        std::cout << e.what() << std::endl; 
     }
 
-    Vector<double> vecOld(xarr.size()); // At time n 
-    Vector<double> vecNew(xarr.size()); // At time n+1
-    // Initial condition
-    for (std::size_t j = 0; j < vecOld.size(); j++) {
-        vecOld[j] = InitialCondition(xarr[j]);
-    }
-    // We start at 1st time point
-    double current = k;
+    // Numerical Differentiation
+    auto derivs = csi.ExtendedSolve(xvar);
+    std::cout << "Derivatives, approx: " << std::get<0>(derivs) << ", "
+    << std::get<1>(derivs) << ", "<< std::get<2>(derivs) << std::endl;
+    std::cout << "Derivatives, exact: " << Pdf(xvar) << ", "
+    << dPdfdx(xvar) << ", " << d2Pdfdx2(xvar) << std::endl;
+    // 1st derivative
+    xvar = dist(eng);
+    std::cout << "1st derivative: " << xvar << ", " << dPdfdx(xvar)
+    << ", " << csi.Derivative(xvar) << std::endl;
 
-    auto r_fun = [&vecOld, lambda, theta] (Vector<double> &r) -> void {
-        for (std::size_t j = 1; j < r.size() - 1; ++j) {
-            r[j] = (lambda * (1.0 - theta) * vecOld[j + 1])
-            + (1.0 - (2.0 * lambda * (1.0 - theta))) * vecOld[j]
-            + (lambda * (1.0 - theta) * vecOld[j - 1]);
-        }
-    };
-    
-    LUTridiagonalSolver<double> mySolver(a, b, c, r, r_fun);
-    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
-    while (current <= T) {
-     // Update at new time level n+1
-        // Compute inhomogeneous term
-        vecNew = mySolver.solve();
-        vecOld = vecNew;
-        current += k;
-    }
-    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-    std::cout << "Elapsed time:  " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << "us." << std::endl;
-
+    return 0;
 }
