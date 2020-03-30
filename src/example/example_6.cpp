62836051c47be78606262f7fcb8b9230f3da6719
j8xixo12 <as2266317@gmail.com>
2020-03-30 21:52:46 +0800

    Add example_6

---
+++ src/example/example_6.cpp
@@ -0,0 +1,38 @@
+#include <random>
+#include <iostream>
+#include <fstream>
+#include "utility.hpp"
+#include "SdeFactory.hpp"
+#include "Option.hpp"
+#include "FdmEuler.hpp"
+#include "FdmHeun.hpp"
+
+int main(int argc, char* argv[]) {
+    std::ofstream output("out.csv", std::ios::out);
+
+    Option myOption(100.0, 10.0, 0.1, 0.9);
+    double S0 = 20.0;
+    // B. Get the SDE
+    // Using factories
+    std::shared_ptr<Sde<double>> sde = SdeFactory<double>::GetSde (myOption, S0, CEV);
+
+    // C. Set up the FDM classes; 1 to N relationship between SDE and FDM 
+    std::default_random_engine eng;
+    std::normal_distribution<double> nor(0.0, 1.0); 
+    std::random_device rd;
+    eng.seed(rd());
+
+    FdmEuler<double> fdm(sde, nor, eng);
+    FdmHeun<double> fdm2(sde, nor, eng);
+    // D. Joining up SDE and FDM in Mediator class 
+    long NT = 200000;
+    // auto vec = Path(*sde, fdm, NT);
+    auto vec2 = Path(*sde, fdm2, NT);
+
+    for (std::size_t i = 0; i < vec2.size(); ++i) {
+        output << vec2[i] << std::endl;
+    }
+    output.close();
+
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,42 @@
-#include <random>
-#include <iostream>
-#include <fstream>
-#include "utility.hpp"
-#include "SdeFactory.hpp"
-#include "Option.hpp"
-#include "FdmEuler.hpp"
-#include "FdmHeun.hpp"
+#include "OptionCommand.hpp"
+#include "CompositeCommand.hpp"
+
+
+
+void TestCP() { // Haug 2007 page 3, C
+    double K = 65.0; 
+    double T = 0.25; 
+    double r = 0.08; 
+    double b = r; 
+    double sig = 0.3;
+    double S = 60.0;
+
+    CallPrice cp(K, T, r, b, sig); 
+    cp.execute(S);
+}
+
+
+void TestCompositeCommand() { // Create a composite option command
+    double K = 65.0; 
+    double T = 0.25; 
+    double r = 0.08; 
+    double b = r; 
+    double sig = 0.3;
+    double S = 60.0;
+    // Create composite command 
+    std::cout << "Command on options\n"; 
+    CompositeCommand optionCommand;
+    // Add prices and their deltas 
+    optionCommand.add(new CallPrice (K, T, r, b, sig)); 
+    // optionCommand.add(new PutPrice(K, T, r, b, sig));
+    optionCommand.add(new CallDelta(K, T, r, b, sig)); 
+    // optionCommand.add(new PutDelta(K, T, r, b, sig));
+    std::cout << "Size of commandlist: " << optionCommand.size();
+    optionCommand.execute(S);
+}
 
 int main(int argc, char* argv[]) {
-    std::ofstream output("out.csv", std::ios::out);
-
-    Option myOption(100.0, 10.0, 0.1, 0.9, 0.03);
-    double S0 = 20.0;
-    // B. Get the SDE
-    // Using factories
-    std::shared_ptr<Sde<double>> sde = SdeFactory<double>::GetSde (myOption, S0, CEV);
-
-    // C. Set up the FDM classes; 1 to N relationship between SDE and FDM 
-    std::default_random_engine eng;
-    std::normal_distribution<double> nor(0.0, 1.0); 
-    std::random_device rd;
-    eng.seed(rd());
-
-    FdmEuler<double> fdm(sde, nor, eng);
-    FdmHeun<double> fdm2(sde, nor, eng);
-    // D. Joining up SDE and FDM in Mediator class 
-    long NT = 200000;
-    // auto vec = Path(*sde, fdm, NT);
-    auto vec2 = Path(*sde, fdm2, NT);
-
-    for (std::size_t i = 0; i < vec2.size(); ++i) {
-        output << vec2[i] << std::endl;
-    }
-    output.close();
-
+    TestCP();
+    TestCompositeCommand();
     return 0;
 }
