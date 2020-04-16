00b71eb2843bd02d792b439e21dfdb827c3c4448
j8xixo12 <as2266317@gmail.com>
2020-04-16 12:35:49 +0800

    Add Monte-Carlo simulation example

--- src/main.cpp
+++ src/main.cpp
@@ -1,115 +1,97 @@
-#include <thread>
 #include <iostream>
-#include <chrono>
-#include "CRRLattice.hpp"
-#include "Lattice.hpp"
+#include <memory>
+#include <random>
+#include <fstream>
+#include "SdeFactory.hpp"
 #include "Option.hpp"
 
-using namespace LatticeMechanism;
-
-auto elapsed(const std::chrono::steady_clock::time_point time1, const std::chrono::steady_clock::time_point time2) {
-    return std::chrono::duration_cast<std::chrono::seconds>(time2 - time1).count();
-}
 
 int main(int argc, char* argv[]) {
-    std::cout << std::thread::hardware_concurrency() << " processors/cores detected." <<'\n';
+
     Option opt;
     opt.K_ = 65.0;
-    opt.T_ = 0.25;
-    opt.r_ = 0.08;
-    opt.sig_ = 0.3;
+    opt.T_ = 1.0;
+    opt.sig_ = 0.4;
+    opt.r_  = 0.02;
 
-    int N = 7000;
+    std::ofstream output("out.csv", std::ios::out);
 
-    double dt = opt.T_ / static_cast<double>(N);
-    std::cout << "Stress test, number of time steps: " << N << '\n';
+    auto PutPayoff = [&opt] (double S)-> double {return std::max<double> (opt.K_ - S, 0.0);};
 
-    CRRLatticeAlgorithms algo(opt, dt);
 
-    // Create basic asset lattice
-    Lattice<double, 2> asset(N, 0.0); // init
-    double rootVal = 60.0; // S(0) 
-    ForwardInduction<double, 2>(asset, algo, rootVal);
+    double S_0 = 60.0;
+    std::shared_ptr<Sde<double>> sde_ptr = SdeFactory<double>::GetSde(opt, S_0, SdeType::GBM);
+    
+    double x;
+    double VOld = S_0;
+    double VNew;
+    long NT = 1000;
+    std::cout << "Number of time steps: " << NT << std::endl;
+    long NSIM = 50;
+    std::cout << "Number of simulations: " << NSIM << std::endl;
 
-    double K = opt.K_;
-    auto PutPayoff = [&K] (double S)-> double { return std::max<double>(K - S, 0.0); }; 
-    auto CallPayoff = [&K] (double S)-> double { return std::max<double>(S - K, 0.0); };
+    std::vector<std::vector<double>> v_data(NT, std::vector<double>(NSIM, 0.0));
 
-    // American early exercise constraint
-    auto AmericanPutAdjuster = [&PutPayoff](double& V, double S)->void { // e.g. early exercise
-        V = std::max<double>(V, PutPayoff(S));
-    };
-    auto AmericanCallAdjuster = [&CallPayoff](double& V, double S)->void { // e.g. early exercise
-        V = std::max<double>(V, CallPayoff(S));
-    };
-    auto EmptyAdjuster = [](double& V, double S)->void { // No action executed at a node
-        // Do nothing, no code generated in client code
-    };
+    double M = static_cast<double>(NSIM);
+    double dt = opt.T_ / static_cast<double> (NT); 
+    double sqrdt = std::sqrt(dt);
 
-    std::chrono::steady_clock::time_point time1 = std::chrono::steady_clock::now();
+    // Normal random number
+    double dW;
+    double price = 0.0; // Option price 
+    double payoffT;
+    double avgPayoffT = 0.0;
+    double squaredPayoff = 0.0;
+    double sumPriceT = 0.0;
+    // Normal (0,1) rng
+    std::default_random_engine dre; 
+    std::normal_distribution<double> nor(0.0, 1.0);
+    // Create a random number
+    dW = nor(dre);
+    long coun = 0; // Number of times S hits origin
+    
+    for (long i = 0; i < M; ++i) {
+        if ((i / 100'00) * 100'00 == i) {// Give status after each 10000th iteration
+            std::cout << i << std::endl;
+        }
 
-    Lattice<double, 2> euroPut(N, 0.0); 
-    ForwardInduction<double, 2>(euroPut, algo, rootVal); 
-    Lattice<double, 2> euroCall(N, 0.0); 
-    ForwardInduction<double, 2>(euroCall, algo, rootVal); 
-    Lattice<double, 2> earlyPut(N, 0.0); 
-    ForwardInduction<double, 2>(earlyPut, algo, rootVal); 
-    Lattice<double, 2> earlyCall(N, 0.0); 
-    ForwardInduction<double, 2>(earlyCall, algo, rootVal);
+        VOld = S_0;
+        x = 0.0;
 
-    double euroPutPrice = BackwardInduction<double>
-            (asset, euroPut, algo, PutPayoff, EmptyAdjuster);
-    std::cout << "Euro put: " << euroPutPrice << std::endl;
-    double euroCallPrice = BackwardInduction<double>
-              (asset, euroCall, algo, CallPayoff, EmptyAdjuster);
-    std::cout << "Euro call: " << euroCallPrice << std::endl;
+        for (long index = 0; index < NT; ++index) {
+            // Create a random number
+            dW = nor(dre);
+            // The FDM (in this case explicit Euler) 
+            VNew = VOld + (dt * sde_ptr->drift(x, VOld))
+                    + (sqrdt * sde_ptr->diffusion(x, VOld) * dW);
+            VOld = VNew;
+            x += dt;
+            v_data[index][i] = VNew;
+        }
 
-    // Price an early exercise option
-    double earlyPutPrice = BackwardInduction<double>
-           (asset, earlyPut, algo, PutPayoff,AmericanPutAdjuster);
-    std::cout << "Early put: " << earlyPutPrice << std::endl;
-    double earlyCallPrice = BackwardInduction<double>
-           (asset, earlyCall, algo, CallPayoff,AmericanCallAdjuster);
-    std::cout << "Early call: " << earlyCallPrice << std::endl;
+        payoffT = PutPayoff(VNew); 
+        sumPriceT += payoffT;
+        avgPayoffT += payoffT / M;
+        avgPayoffT *= avgPayoffT;
+        squaredPayoff += (payoffT * payoffT);
+    }
 
-    std::chrono::steady_clock::time_point time2 = std::chrono::steady_clock::now();
-    std::cout << "Elapsed time sequential code: " << elapsed(time1, time2) << std::endl;
-
-    double Euro_put, Euro_call, Early_put, Early_call;
-    auto fn1 = [&]() {
-        ForwardInduction<double> (euroPut, algo, rootVal);
-        Euro_put = BackwardInduction<double> (asset, euroPut, algo, PutPayoff, EmptyAdjuster);
-    };
-    auto fn2 = [&]() {
-        ForwardInduction<double> (euroCall, algo, rootVal);
-        Euro_call = BackwardInduction<double> (asset, euroCall, algo, CallPayoff, EmptyAdjuster);
-    };
-    auto fn3 = [&]() {
-        ForwardInduction<double> (earlyPut, algo, rootVal);
-        Early_put = BackwardInduction<double> (asset, earlyPut, algo, PutPayoff, AmericanPutAdjuster);
-    };
-    auto fn4 = [&]() {
-        ForwardInduction<double> (earlyCall, algo, rootVal);
-        Early_call = BackwardInduction<double> (asset, earlyCall, algo, CallPayoff, AmericanCallAdjuster);
-    };
-
-    time1 = std::chrono::steady_clock::now();
-    std::cout << '\n';
-    std::thread t1(fn1);
-    std::thread t2(fn2);
-    std::thread t3(fn3);
-    std::thread t4(fn4);
-    // No shared data so we define 1 barrier/rendezvous 
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
-    time2 = std::chrono::steady_clock::now();
-    std::cout << "Euro put: " << Euro_put << std::endl;
-    std::cout << "Euro call: " << Euro_call << std::endl;
-    std::cout << "Early put: " << Early_put << std::endl;
-    std::cout << "Early call: " << Early_call << std::endl;
-    std::cout << "Elapsed time C++11 threads in seconds: " << elapsed(time1, time2) << std::endl;
+    for (long index = 0; index < NT; ++index) {
+        output << index * dt << ",";
+        for (long i = 0; i < M; i++) {
+            output << v_data[index][i] << ","; 
+        }
+        output << std::endl;
+    }
+    
+    output.close();
+    // Finally, discounting the average price
+    price = std::exp(-opt.r_ * opt.T_) * sumPriceT / M;
+    std::cout << "Price, after discounting: " << price << ", " << std::endl;
+    double SD = std::sqrt((squaredPayoff / M) - sumPriceT * sumPriceT / (M * M));
+    std::cout << "Standard Deviation: " << SD << ", " << std::endl;
+    double SE = SD / std::sqrt(M);
+    std::cout << "Standard Error: " << SE << ", " << std::endl;
 
     return 0;
 }
